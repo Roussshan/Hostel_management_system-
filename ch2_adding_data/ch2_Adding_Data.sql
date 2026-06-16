395eff01da5020dc9f89a80d37b472dc2327c9e8
Roushan kumar <roussshan@gmail.com>
2026-06-16 23:33:04 +0530

    update ch2 and add ch3 with different types of Query

--- ch2_adding_data/ch2_Adding_Data.sql
+++ ch2_adding_data/ch2_Adding_Data.sql
@@ -23,24 +23,3 @@ VALUES
 -- Verify all data was inserted correctly
 SELECT * FROM students;
 
--- ------------------------------------------------
--- Querying data with using 'SELECT' and 'WHERE'.
--- ------------------------------------------------
-
-	SELECT * from students WHERE gender = 'male';
-    SELECT * from students WHERE  gender != 'male';
-    
-    SELECT * from students WHERE age < '20';
-    SELECT * from students WHERE age > '20';
-    SELECT * from students WHERE age <= '20';
-	SELECT * from students WHERE age >= '20';
-
-    SELECT * from students WHERE gender = 'male' and age < '20';
-    SELECT * from students WHERE gender = 'male' or age < '20';
-
-    SELECT * from students ORDER BY age asc;
-	SELECT * from students ORDER BY age desc;
-
-	SELECT * from students limit 5; -- for first 5 rows 
-    SELECT * from students limit 10 offset 5; -- skip first 5 rows then get next 10.
-    SELECT * from students limit 5, 10; -- gets the 10 row starting from the 6th row. 
