00abbe1db69939daeba8f7a1075f35bb298ed8aa
Roushan kumar <roussshan@gmail.com>
2026-06-16 22:16:31 +0530

    Querying data form the student table.

--- ch2_adding_data/ch2_Adding_Data.sql
+++ ch2_adding_data/ch2_Adding_Data.sql
@@ -1,6 +1,7 @@
 
 use hostel_management;
 
+-- ------------------------------------------------
 -- INSERT sample student data into students table
 -- Note: student_id is AUTO_INCREMENT, so we skip it
 -- ------------------------------------------------
@@ -22,4 +23,24 @@ VALUES
 -- Verify all data was inserted correctly
 SELECT * FROM students;
 
+-- ------------------------------------------------
+-- Querying data with using 'SELECT' and 'WHERE'.
+-- ------------------------------------------------
 
+	SELECT * from students WHERE gender = 'male';
+    SELECT * from students WHERE  gender != 'male';
+    
+    SELECT * from students WHERE age < '20';
+    SELECT * from students WHERE age > '20';
+    SELECT * from students WHERE age <= '20';
+	SELECT * from students WHERE age >= '20';
+
+    SELECT * from students WHERE gender = 'male' and age < '20';
+    SELECT * from students WHERE gender = 'male' or age < '20';
+
+    SELECT * from students ORDER BY age asc;
+	SELECT * from students ORDER BY age desc;
+
+	SELECT * from students limit 5; -- for first 5 rows 
+    SELECT * from students limit 10 offset 5; -- skip first 5 rows then get next 10.
+    SELECT * from students limit 5, 10; -- gets the 10 row starting from the 6th row. 
