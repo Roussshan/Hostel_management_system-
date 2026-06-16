c78d665a5af619f9ece83d2a99ecaaaa0bbdfacd
Roushan kumar <roussshan@gmail.com>
2026-06-16 23:54:24 +0530

    Update ch3_querying_data

--- ch3_querying_data/ch3_querying_data.sql
+++ ch3_querying_data/ch3_querying_data.sql
@@ -15,15 +15,16 @@ use hostel_management;
     SELECT * from students WHERE gender = 'male' and age < '20';
     SELECT * from students WHERE gender = 'male' or age < '20';
 
-    SELECT * from students ORDER BY age asc;
+-- use ORDER BY - which hepls in arranging the data in an order.
+    SELECT * from students ORDER BY age asc;  
 	SELECT * from students ORDER BY age desc;   
 
+-- use LIMIT in the table which helps in set the limit in the output.
 	SELECT * from students limit 5; -- for first 5 rows 
     SELECT * from students limit 10 offset 5; -- skip first 5 rows then get next 10.
     SELECT * from students limit 5, 10; -- gets the 10 row starting from the 6th row. 
     
-    -- update data in table.
-    
+-- use UPDATE in the table which hepls in updating the rows in the table.
     UPDATE students
     set fee_status = 'paid' where student_id = '2';
     
