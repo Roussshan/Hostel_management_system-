8eba88e319fd60991761a3328749d4588db5faaa
Roushan kumar <roussshan@gmail.com>
2026-06-17 23:59:32 +0530

    Update ch4_modifying_data
    use UPDATE  to Update the existing table data.

--- ch4_modifying_data/ch4_modifying_data.sql
+++ ch4_modifying_data/ch4_modifying_data.sql
@@ -3,5 +3,13 @@ use hostel_management;
 -- modify data of the table by using UPDATE and DELETE 
 
 -- UPDATE - helps in change the valuses in one and more rows.
--- DELETE - helps in removing the data from the table. 
+-- DELETE - helps in removing the data from the table.
 
+-- use UPDATE which helps in updateing the rows in the table.
+	UPDATE students 
+	set room_number = '109' , fee_status = 'due' Where student_id = '1';
+    UPDATE students 
+    set room_number = '101' , fee_status = 'due' Where student_id = '2';
+
+
+    select * from students;
