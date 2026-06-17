f97999bf1f52c684962b55d24d3d7078adeb73ee
Roushan kumar <roussshan@gmail.com>
2026-06-17 23:46:26 +0530

    Create ch4_modifying_data
    In this chapter we are going tho do hands on on how to update and delete the data from your MySql database table.

---
+++ ch4_modifying_data/ch4_modifying_data.sql
@@ -0,0 +1,7 @@
+use hostel_management;
+
+-- modify data of the table by using UPDATE and DELETE 
+
+-- UPDATE - helps in change the valuses in one and more rows.
+-- DELETE - helps in removing the data from the table. 
+
