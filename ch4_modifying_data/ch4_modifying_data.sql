04fc2c6b36ec92eb58be78f3bcc2aef860dcfbc3
Roushan kumar <roussshan@gmail.com>
2026-06-18 15:01:16 +0530

    update ch4_modifying_data
    In this we are going to do some hands on with 'DELETE' and learn how the 'DELETE' works.

--- ch4_modifying_data/ch4_modifying_data.sql
+++ ch4_modifying_data/ch4_modifying_data.sql
@@ -5,11 +5,24 @@ use hostel_management;
 -- UPDATE - helps in change the valuses in one and more rows.
 -- DELETE - helps in removing the data from the table.
 
+-- Again inserting some data into table.
+   INSERT into students (student_id, name, age, gender, room_number, fee_status) values 
+   ('5','kunal', '21', 'male', '119', 'paid'),
+   ('11','sneha', '23', 'female', '121', 'paid'),
+   ('12','pooja', '20', 'female', '001', 'due');
+
 -- use UPDATE which helps in updateing the rows in the table.
 	UPDATE students 
 	set room_number = '109' , fee_status = 'due' Where student_id = '1';
     UPDATE students 
     set room_number = '101' , fee_status = 'due' Where student_id = '2';
+	UPDATE students 
+    set room_number = '104' Where student_id = '3';
 
-
-    select * from students;
+-- use DELETE which helps in deleating thef row/data from the table.
+    DELETE from students -- delete one row.
+    where student_id = '5';
+    DELETE from students -- delete multiple rows.
+    where gender = 'other';
+    
+    select * from students;  
