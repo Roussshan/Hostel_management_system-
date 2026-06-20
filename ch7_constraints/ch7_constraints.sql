fcaa7d5cceed7de15d770314a8511ea12e00c482
Roushan kumar <roussshan@gmail.com>
2026-06-20 23:52:05 +0530

    Updating ch7_constraints

--- ch7_constraints/ch7_constraints.sql
+++ ch7_constraints/ch7_constraints.sql
@@ -6,12 +6,19 @@ describe students;
 -- --------------------------------------------
 
 -- Add constraint
+
    ALTER TABLE students 
    add constraint unique_email unique (email); 
-
--- verifying 
+   -- verifying 
    update students 
    set email = 'aarav@gmail.com' where student_id = '2';
-   -- ok its working.
+   -- should fail with "Duplicate entry"
+   
+   ALTER TABLE students
+   add constraint chk_age check (age > 16);
+   -- verifing
+   INSERT INTO students (Name, Age, Gender, Room_number, Fee_status, email)
+   VALUES ('Young Test', 10, 'Male', 101, 'Due', 'young@example.com');
+   -- should fail the CHECK
 
 select * from students;
