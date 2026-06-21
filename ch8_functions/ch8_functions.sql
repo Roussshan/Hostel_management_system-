a88865cdd10ea179f88f9fc7a9406d5406f1d852
Roushan kumar <roussshan@gmail.com>
2026-06-21 21:10:52 +0530

    Update ch8_functions
    hands on with COUNT().

--- ch8_functions/ch8_functions.sql
+++ ch8_functions/ch8_functions.sql
@@ -4,4 +4,12 @@ use hostel_management;
 -- using MySQL functions
 -- ----------------------
 
+-- There are many types of My SQL functions :
+-- 1. Aggregrate Functions - (COUNT(), MIN(), MAX(), SUM(), AVG(), GROUP BY())
+
+-- Using COUNT()
+   SELECT COUNT(*) from students; -- helps to count total numbers of students.
+   SELECT COUNT(*) from students where gender = 'female'; -- helps to count the total numbers of female.
+   SELECT COUNT(*) from students where gender = 'male'; -- helps to count the total numbers of male.
+   
 select * from students;
