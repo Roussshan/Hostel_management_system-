ff0dd244f5970288551100088ae36ce4877f98cb
Roushan kumar <roussshan@gmail.com>
2026-06-22 22:29:57 +0530

    Update ch10_room_relation
    Adding room_numbers (which is already present in students table) to new room table.

--- ch10_rooms_relation/ch10_room_relation.sql
+++ ch10_rooms_relation/ch10_room_relation.sql
@@ -1,6 +1,8 @@
 use hostel_management;
 
+-- ----------------------- --
 -- creating new room table --
+-- ----------------------- --
 
 CREATE TABLE rooms (
     room_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -9,3 +11,16 @@ CREATE TABLE rooms (
 );
 
 select * from rooms; -- shows the room table 
+select * from students; -- shows the students table
+
+SELECT DISTINCT Room_number FROM students; -- shows you the unique room numbers currently sitting inside your students table
+
+-- Adding those room to new room table.
+   INSERT INTO rooms (room_number, capacity) VALUES (101, 2);
+   INSERT INTO rooms (room_number, capacity) VALUES (102, 2); 
+   INSERT INTO rooms (room_number, capacity) VALUES (103, 2);
+   INSERT INTO rooms (room_number, capacity) VALUES (104, 2);
+   INSERT INTO rooms (room_number, capacity) VALUES (105, 2);
+   INSERT INTO rooms (room_number, capacity) VALUES (119, 3);
+   INSERT INTO rooms (room_number, capacity) VALUES (121, 3);
+   INSERT INTO rooms (room_number, capacity) VALUES (111, 3);
