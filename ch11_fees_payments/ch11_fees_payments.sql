8251c06fe40783db93592fcb940080593fdcfb38
Roushan kumar <roussshan@gmail.com>
2026-06-23 22:39:08 +0530

    Update ch11_fees_payments
    Inserting fees row.

--- ch11_fees_payments/ch11_fees_payments.sql
+++ ch11_fees_payments/ch11_fees_payments.sql
@@ -30,3 +30,21 @@ CREATE TABLE payments (
 );
 
 DESCRIBE payments;
+
+-- Inserting fee row per student.
+INSERT INTO fees (student_id, amount_due, due_date, status) VALUES
+(1, 8000, '2026-06-01', 'Paid'),
+(2, 8000, '2026-06-01', 'Due'),
+(3, 8000, '2026-06-01', 'Paid'),
+(4, 8000, '2026-06-01', 'Paid'),
+(5, 8000, '2026-06-01', 'Due'),
+(6, 8000, '2026-06-01', 'Paid'),
+(7, 8000, '2026-06-01', 'Due'),
+(8, 8000, '2026-06-01', 'Paid'),
+(9, 8000, '2026-06-01', 'Paid'),
+(10, 8000, '2026-06-01', 'Due'),
+(11, 8000, '2026-06-01', 'Paid'),
+(12, 8000, '2026-06-01', 'Paid'),
+(13, 8000, '2026-06-01', 'Due');
+
+select * from fees;
