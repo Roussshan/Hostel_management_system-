9ec3b882d9a4d12f33bac8a494002c368e3bf64c
Roushan kumar <roussshan@gmail.com>
2026-06-24 22:58:07 +0530

    Update ch13_self_join
    Running self join.

--- ch13_self_join/ch13_self_join.sql
+++ ch13_self_join/ch13_self_join.sql
@@ -11,3 +11,13 @@ UPDATE students SET referred_by_id = 1 WHERE student_id IN (2, 3);   -- Aarav re
 UPDATE students SET referred_by_id = 2 WHERE student_id = 4;          -- Priya referred Sneha
 UPDATE students SET referred_by_id = 6 WHERE student_id IN (11, 12);  -- Anjali referred Kunal and Sneha Bansal
 -- Leave the rest as NULL (not referred by anyone)
+
+SELECT student_id, Name, referred_by_id FROM students ORDER BY student_id; -- verify
+
+-- Running self Join
+SELECT 
+    a.student_id,
+    a.Name AS student_name,
+    b.Name AS referred_by
+FROM students a
+LEFT JOIN students b ON a.referred_by_id = b.student_id;
