d939dda5d8eba748d678202bfb3472e2ab7b7c1a
Roushan kumar <roussshan@gmail.com>
2026-06-25 22:25:36 +0530

    Create ch14_joins

---
+++ ch14_joins/ch14_joins.sql
@@ -0,0 +1,12 @@
+USE hostel_management;
+
+-- 2-table joins already done individually
+-- Students with their room
+SELECT s.Name, r.room_number
+FROM students s
+JOIN rooms r ON s.room_id = r.room_id;
+
+-- Students with their fee status
+SELECT s.Name, f.amount_due, f.status
+FROM students s
+JOIN fees f ON s.student_id = f.student_id;
