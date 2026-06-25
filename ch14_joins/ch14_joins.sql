c5d2fe4c53679284c2aae49799347ed05a4d8ba1
Roushan kumar <roussshan@gmail.com>
2026-06-25 22:27:32 +0530

    Update ch14_joins
    Hands on with inner join

--- ch14_joins/ch14_joins.sql
+++ ch14_joins/ch14_joins.sql
@@ -10,3 +10,9 @@ JOIN rooms r ON s.room_id = r.room_id;
 SELECT s.Name, f.amount_due, f.status
 FROM students s
 JOIN fees f ON s.student_id = f.student_id;
+
+-- INNER JOIN Across 3 tables
+SELECT s.Name, r.room_number, f.status AS fee_status
+FROM students s
+INNER JOIN rooms r ON s.room_id = r.room_id
+INNER JOIN fees f ON s.student_id = f.student_id;
