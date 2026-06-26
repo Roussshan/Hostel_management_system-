75fda0da7cf8fa9d49b44b0e8eabe71854f69317
Roushan kumar <roussshan@gmail.com>
2026-06-26 22:53:04 +0530

    Update ch15_views
    Turn it into a view.

--- ch15_views/ch15_views.sql
+++ ch15_views/ch15_views.sql
@@ -13,3 +13,18 @@ FROM students s
 JOIN fees f ON s.student_id = f.student_id
 LEFT JOIN payments p ON f.fee_id = p.fee_id
 GROUP BY s.student_id, s.Name, f.amount_due;
+
+-- turn it into View
+CREATE VIEW student_outstanding_dues AS
+SELECT 
+    s.student_id,
+    s.Name,
+    f.amount_due,
+    COALESCE(SUM(p.amount), 0) AS amount_paid,
+    f.amount_due - COALESCE(SUM(p.amount), 0) AS balance
+FROM students s
+JOIN fees f ON s.student_id = f.student_id
+LEFT JOIN payments p ON f.fee_id = p.fee_id
+GROUP BY s.student_id, s.Name, f.amount_due;
+
+SELECT * FROM student_outstanding_dues;
