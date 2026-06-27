9af13a34d54d494db139ae61cbacd7a17f3f8e1b
Roushan kumar <roussshan@gmail.com>
2026-06-27 21:54:52 +0530

    Update ch16_indexes
    create a single column index.

--- ch16_indexes/ch16_indexes.sql
+++ ch16_indexes/ch16_indexes.sql
@@ -1,5 +1,13 @@
 USE hostel_management;
+
+-- Check existing indexes first
+
 SHOW INDEXES FROM students;
 SHOW INDEXES FROM rooms;
 SHOW INDEXES FROM fees;
 SHOW INDEXES FROM complaints;
+
+-- Create a single-column index
+CREATE INDEX idx_name ON students(Name);
+SHOW INDEXES FROM students; -- verify
+
